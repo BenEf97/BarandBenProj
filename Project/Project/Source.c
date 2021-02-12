746cb36e8250893dfdbf90719afd75c0f3c720da
BenEf97 <benefrath@gmail.com>
2021-02-12 14:41:11 +0200

    Added print_person, There is an error printing the date.

--- Project/Project/Source.c
+++ Project/Project/Source.c
@@ -7,6 +7,7 @@ typedef struct{
 	char month;
 	char day;
 }DateOfBirth;
+//I think we might need to make it as a pointer bc it might have more digits.
 
 typedef struct{
 	long id;
@@ -17,22 +18,34 @@ typedef struct{
 	long MotherId;
 	long FatherId;
 	char NumOfChildren;
-	long *childrenPtr
+	long *childrenPtr;
 }person;
+
+void print_person(person* per);
 
 void main()
 {
-	
+	person test = { 123456789,"John","Cena",{"1","2",1990},2345654,1345678,'0' };
+	print_person(&test);
 }
 
+//Prints the person's info
 void print_person(person* per)
 {
 	printf("ID: %ld\n", per->id);
 	printf("First Name: %s\n", per->name);
 	printf("Last Name: %s\n",per->family);
-	printf("Date: %s/%s/%d \n",per->date.day,per->date.month,per->date.year);
+	printf("Date: %s/%s/%d \n",per->date.day,per->date.month,per->date.year);//error
 	printf("Partner's ID: %ld\n",per->partnerId);
 	printf("Mother's ID: %ld\n", per->MotherId);
 	printf("Father's ID: %ld\n", per->FatherId);
-
+	printf("Number of children: %s\n", per->NumOfChildren); //what about 2 digits
+	if ((per->NumOfChildren) != '0')
+	{
+		for (int idx = 0; idx < per->NumOfChildren; idx++)
+		{
+			printf("Child No. %d:\n", idx + 1);
+			printf("ID: %ld\n", per->childrenPtr[idx]);
+		}
+	}
 }
