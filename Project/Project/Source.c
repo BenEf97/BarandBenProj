26ff4563776117e8117f03ceb2b97e52e1e7719b
BenEf97 <benefrath@gmail.com>
2021-02-12 13:26:24 +0200

    Added person struct

--- Project/Project/Source.c
+++ Project/Project/Source.c
@@ -2,8 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct{
+	int year;
+	char month;
+	char day;
+}DateOfBirth;
+
+typedef struct{
+	long id;
+	char* name;
+	char* family;
+	DateOfBirth date;
+	long partnerId;
+	long MotherId;
+	long FatherId;
+	char NumOfChildren;
+	long *childrenPtr
+}person;
 
 void main()
 {
+	
+}
+
+void print_person(person* per)
+{
+	printf("ID: %ld\n", per->id);
+	printf("First Name: %s\n", per->name);
+	printf("Last Name: %s\n",per->family);
+	printf("Date: %s/%s/%d \n",per->date.day,per->date.month,per->date.year);
+	printf("Partner's ID: %ld\n",per->partnerId);
+	printf("Mother's ID: %ld\n", per->MotherId);
+	printf("Father's ID: %ld\n", per->FatherId);
 
 }
