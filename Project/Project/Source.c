2b68fb22a2672bbb00e1d53ae1cceef6996195d5
BenEf97 <benefrath@gmail.com>
2021-02-14 12:44:57 +0200

    minor update

--- Project/Project/Source.c
+++ Project/Project/Source.c
@@ -31,8 +31,8 @@ typedef struct {
 
 void print_person(person* per);
 //char menu();
-//person* init_db(db_mgr* mgr1);
-void init_db(db_mgr* mgr);
+person* init_db(db_mgr* mgr1);
+//void init_db(db_mgr* mgr);
 char intAsChar(int a);
 void add_person(db_mgr* mgr);
 char* enterName(char* name);
@@ -77,57 +77,44 @@ char intAsChar(int a) {  //puts the value of int in char
 	return b;
 }
 
-void init_db(db_mgr* mgr1) //i think we should use a void
+person* init_db(db_mgr* mgr1)
 { //check me dont know if im right  
 	db_mgr mgr2;
 	if (mgr1->perCount > 0)
 	{
 		mgr2.per = (person*)malloc(mgr1->perCount * sizeof(person));
 		for (int i = 0; i < mgr1->perCount; i++) {
-			mgr2.per[i] = mgr1->per[i]; //error
+			//mgr2.per[i].id = mgr1->per[i].id; //error
+			swapPosition(&mgr2.per[i], &mgr1->per[i]);
 		}
 	free(mgr1->per);
 	}
-//	return mgr2.per;
+	return mgr2.per;
 }
 
-//void init_db (db_mgr* mgr)
-//{
-//	if (mgr->perCount > 0)
-//	{
-//		mgr->per = (person*)malloc(mgr->perCount * sizeof(person));
-//	}
-//}
+
 
 //the function didn't work so rn just wanna check without it
-//char menu()
-//{ //menu function returns the value that entered
-//	int tmp = 1;
-//	char choice;
-//	while (tmp){
-//		printf("Database System Menu:\n1. Add person\n2. Search a person\n3. Search parents\n4. Delete a person\n5. Get generation\n6. Print database\n7. Search by name\n8. Quit\n");
-//		scanf("%c",&choice);
-//		fseek(stdin, 0, SEEK_END);
-//		switch (choice) {
-//		case '1': return choice;
-//		case '2': return choice;
-//		case '3': return choice;
-//		case '4': return choice;
-//		case '5': return choice;
-//		case '6': return choice;
-//		case '7': return choice;
-//		case '8': return choice;
-//		default: printf("error! invalid input. try again.\n");
-//		}
-//	}
-//}
+char menu()
+{
+	//menu function returns the value that entered
+		int tmp = 1;
+	char choice;
+	while (tmp)
+	{
+		printf("Database System Menu:\n1. Add person\n2. Search a person\n3. Search parents\n4. Delete a person\n5. Get generation\n6. Print database\n7. Search by name\n8. Quit\n");
+		scanf("%c", &choice);
+		if (choice > '0'&&choice < '9') return choice;
+		printf("error! invalid input. try again.\n");
+	}
+}
 
 void add_person(db_mgr* mgr)
 { //not finished
 
 	int index = mgr->perCount; //start at 0
 	mgr->perCount++;
-	init_db(mgr);
+	mgr->per = init_db(mgr);
 //	person man;
 	printf("please enter a person details:\n");
 	printf("ID: ");
@@ -139,7 +126,7 @@ void add_person(db_mgr* mgr)
 	//}
 	scanf("%ld", &mgr->per[index].id);
 	fseek(stdin, 0, SEEK_END);
-	while (mgr->per[index].id < 0 || mgr->per[index].id > 1000000000 )
+	while (mgr->per[index].id < 0 && mgr->per[index].id > 1000000000 )
 	{
 		printf("invalid input. try again.");
 		scanf("%ld", &mgr->per[index].id);
@@ -197,9 +184,64 @@ void arrangeId(db_mgr* mgr)
 		}
 }
 
-void swapPosition(person* per1, person* per2)
+void swapPer(person* per1, person* per2)
 {
-	person temp = *per1;
-	*per1 = *per2;
-	*per2 = temp;
+	person temp;
+	temp.id = per1->id;
+	temp.name = per1->name;
+	temp.family = per1->family;
+	temp.date.day = per1->date.day;
+	temp.date.month = per1->date.month;
+	temp.date.year = per1->date.year;
+	temp.partnerId = per1->partnerId;
+	temp.FatherId = per1->FatherId;
+	temp.MotherId = per1->MotherId;
+	temp.NumOfChildren = per1->NumOfChildren;
+	if (per1->NumOfChildren > 0)
+	{
+		for (int idx = 0; idx < per1->NumOfChildren; idx++)
+		{
+			temp.childrenPtr[idx] = per1->childrenPtr[idx];
+		}
+	}
+	else temp.childrenPtr = NULL;
+
+	per1->id = per2->id;
+	per1->name = per2->name;
+	per1->family = per2->family;
+	per1->date.day = per2->date.day;
+	per1->date.month = per2->date.month;
+	per1->date.year = per2->date.year;
+	per1->partnerId = per2->partnerId;
+	per1->FatherId = per2->FatherId;
+	per1->MotherId = per2->MotherId;
+	per1->NumOfChildren = per2->NumOfChildren;
+	if (per2->NumOfChildren > 0)
+	{
+		for (int idx = 0; idx < per2->NumOfChildren; idx++)
+		{
+			per1->childrenPtr[idx] = per2->childrenPtr[idx];
+		}
+	}
+	else per1->childrenPtr = NULL;
+
+	per2->id = temp.id;
+	per2->name = temp.name;
+	per2->family = temp.family;
+	per2->date.day = temp.date.day;
+	per2->date.month = temp.date.month;
+	per2->date.year = temp.date.year;
+	per2->partnerId = temp.partnerId;
+	per2->FatherId = temp.FatherId;
+	per2->MotherId = temp.MotherId;
+	per2->NumOfChildren = temp.NumOfChildren;
+	if (temp.NumOfChildren > 0)
+	{
+		for (int idx = 0; idx < temp.NumOfChildren; idx++)
+		{
+			per2->childrenPtr[idx] = temp.childrenPtr[idx];
+		}
+	}
+	else per2->childrenPtr = NULL;
+
 }
